e018650cc5c7398fae985c3735383afeef56e109
=Anuroop <anuroopks7911@gmail.com>
2024-08-13 14:56:23 +0530

    first commit

--- src/main/java/com/revature/task/configs/WebConfig.java
+++ src/main/java/com/revature/task/configs/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/*") // Allow CORS for all API endpoints
-                        .allowedOrigins("https://reactfrontend-bzhwfub9avdfachg.southindia-01.azurewebsites.net/") // Allow requests from your React app
+                        .allowedOrigins("http://localhost:3000") // Allow requests from your React app
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Allow specific HTTP methods
                         .allowedHeaders("*") // Allow all headers
                         .allowCredentials(true); // Allow credentials (e.g., cookies, authorization headers)
