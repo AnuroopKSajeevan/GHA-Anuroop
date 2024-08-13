666fc80a719c33df4dbb82c32e44df408a235121
=Anuroop <anuroopks7911@gmail.com>
2024-08-13 20:45:18 +0530

    first commit

--- src/main/java/com/revature/task/configs/WebConfig.java
+++ src/main/java/com/revature/task/configs/WebConfig.java
@@ -13,11 +13,11 @@ public class WebConfig {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/api/*") // Allow CORS for all API endpoints
-                        .allowedOrigins("http://localhost:3000") // Allow requests from your React app
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Allow specific HTTP methods
-                        .allowedHeaders("*") // Allow all headers
-                        .allowCredentials(true); // Allow credentials (e.g., cookies, authorization headers)
+                registry.addMapping("/api/*")
+                        .allowedOrigins("https://reactfrontend-bzhwfub9avdfachg.southindia-01.azurewebsites.net")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
             }
         };
     }
