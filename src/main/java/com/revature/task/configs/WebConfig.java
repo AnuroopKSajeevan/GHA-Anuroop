7a4787da1ef0d795fe59669a25bbf09b12f09c77
=Anuroop <anuroopks7911@gmail.com>
2024-08-13 12:25:01 +0530

    cors

--- src/main/java/com/revature/task/configs/WebConfig.java
+++ src/main/java/com/revature/task/configs/WebConfig.java
@@ -13,7 +13,7 @@ public class WebConfig {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/api/**") // Allow CORS for all API endpoints
+                registry.addMapping("/api/*") // Allow CORS for all API endpoints
                         .allowedOrigins("http://localhost:3000") // Allow requests from your React app
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Allow specific HTTP methods
                         .allowedHeaders("*") // Allow all headers
